c49090f74a8d63b230b24539d643298c041b7d1b
Juzcan <56765047+Juzcan@users.noreply.github.com>
2019-11-10 14:56:09 +0100

    CalcFunctions (base)
    die Basis zur "calcfunction" hinzugefügt

---
+++ MadMaths/CalcFunctions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadMaths
+{
+    class CalcFunctions
+    {
+        /* funktions.addieren / funktions.subtrahieren / usw. als berechnung für LÖSUNG */
+    }
+}
