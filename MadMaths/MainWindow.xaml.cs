834a354a7d96df76a667c974a2686a2b4b59ce6a
Tim Thomas <inf3253@hs-worms.de>
2019-11-11 19:35:10 +0100

    Custom Windows Border #2

--- MadMaths/MainWindow.xaml.cs
+++ MadMaths/MainWindow.xaml.cs
@@ -25,18 +25,18 @@ namespace MadMaths
         public MainWindow()
         {
 
-            //if (!Controller.CheckSaveDir())
-            //{
-            //    Controller.CreateSaveDir();
-            //}
-            //if (!Controller.CheckSaveFile())
-            //{
-            //    Controller.CreateUserJS();
-            //}
-            //FileInfo fi = new FileInfo(Controller.UserSaveFile);
-            //fi.Attributes = FileAttributes.Normal;
+            if (!Controller.CheckSaveDir())
+            {
+                Controller.CreateSaveDir();
+            }
+            if (!Controller.CheckSaveFile())
+            {
+                Controller.CreateUserJS();
+            }
+            FileInfo fi = new FileInfo(Controller.UserSaveFile);
+            fi.Attributes = FileAttributes.Normal;
             InitializeComponent();
-            //MainFrame.Source = new Uri("pages/home.xaml", UriKind.Relative); // lädt Homescreen
+            MainFrame.Source = new Uri("pages/home.xaml", UriKind.Relative); // lädt Homescreen
         }
 
         ~MainWindow()
