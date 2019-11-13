3f0328d9cf0cd6f0e4b00e3197c0ffeb9c028a49
Juzcan <56765047+Juzcan@users.noreply.github.com>
2019-11-13 18:10:40 +0100

    Änderung der funktions namen

--- MadMaths/CalcFunctions.cs
+++ MadMaths/CalcFunctions.cs
@@ -9,8 +9,8 @@ namespace MadMaths
     class CalcFunctions
     {
         /* funktions.addieren / funktions.subtrahieren / usw. als berechnung für LÖSUNG */
-        double funktions_addieren(double input_a, double input_b) { return (input_a + input_b); }
-        double funktions_subtrahieren(double input_a, double input_b)
+        double standard_addieren(double input_a, double input_b) { return (input_a + input_b); }
+        double grundschule_subtrahieren(double input_a, double input_b)
         {
             if (input_a > input_b)
             {
@@ -21,8 +21,8 @@ namespace MadMaths
                 return (input_b - input_a);
             }
         }
-        double funktions_multiplizieren(double input_a, double input_b) { return (input_a * input_b);}
-        double funktions_dividieren_glatt(double input_a, double input_b) /* Ohne gleitkomma */
+        double standard_multiplizieren(double input_a, double input_b) { return (input_a * input_b);}
+        double grundschule_dividieren_glatt(double input_a, double input_b) /* Ohne gleitkomma */
         {
             if (input_a % input_b == 0)
             {
@@ -33,11 +33,11 @@ namespace MadMaths
                 return input_b / input_a;
             }
         }
-        double funktions_kindersachtext(double input_a) {
+        double kindersachtext(double input_a) {
             return input_a / 4;
             
         }
-        double funktions_timaufgabe(double input_a, double input_b) /* input_a = Anfangsgeld, input_b = Jahr */
+        double timaufgabe(double input_a, double input_b) /* input_a = Anfangsgeld, input_b = Jahr */
         {
             double zaehler = 0;
             for(int i = 2;i < input_b;i++ )
@@ -47,10 +47,10 @@ namespace MadMaths
             return zaehler + input_a;
         }
 
-        double funktions_timaufgabe_insgesamt(double input_a, double input_b) {
+        double timaufgabe_insgesamt(double input_a, double input_b) {
             double zaehler = 0;
             for (int i = 1; i < input_b; i++) {
-                zaehler += funktions_timaufgabe(input_a, i);
+                zaehler += timaufgabe(input_a, i);
             }
             return zaehler;
         }
