bd8fa49eb532f80cef0509ff7344a5bb89a4caae
Juzcan <56765047+Juzcan@users.noreply.github.com>
2019-11-18 20:43:14 +0100

    oberstufe geändert
    oberstufe inhalt hinzugefügt

--- MadMaths/CalcFunctions_Grundschule.cs
+++ MadMaths/CalcFunctions_Grundschule.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MadMaths
 {
-    class CalcFunctions_Grundschule
+    public class CalcFunctions_Grundschule
     {
         /* funktions.addieren / funktions.subtrahieren / usw. als berechnung für LÖSUNG */
         int standard_addieren(int input_a, int input_b) { return (input_a + input_b); }
@@ -21,7 +21,7 @@ namespace MadMaths
                 return (input_b - input_a);
             }
         }
-        int standard_multiplizieren(int input_a, int input_b) { return (input_a * input_b);}
+        int standard_multiplizieren(int input_a, int input_b) { return (input_a * input_b); }
         int grundschule_dividieren_glatt(int input_a, int input_b) /* Ohne gleitkomma */
         {
             if (input_a % input_b == 0)
@@ -33,27 +33,82 @@ namespace MadMaths
                 return input_b / input_a;
             }
         }
-        int kindersachtext(int input_a) {
+        int kindersachtext(int input_a)
+        {
             return input_a / 4;
-            
+
         }
         int timaufgabe(int input_a, int input_b) /* input_a = Anfangsgeld, input_b = Jahr */
         {
             int zaehler = 0;
-            for(int i = 2;i < input_b;i++ )
+            for (int i = 2; i < input_b; i++)
             {
                 zaehler += i;
             }
             return zaehler + input_a;
         }
 
-        int timaufgabe_insgesamt(int input_a, int input_b) {
+        int timaufgabe_insgesamt(int input_a, int input_b)
+        {
             int zaehler = 0;
-            for (int i = 1; i < input_b; i++) {
+            for (int i = 1; i < input_b; i++)
+            {
                 zaehler += timaufgabe(input_a, i);
             }
             return zaehler;
         }
         int essenaufgabe(int input_a, int input_b) { return (input_b - input_a); }
+
+        string GroesserKleiner1(int input_a, int input_b)
+        {
+            if (input_a == input_b) { return "="; }
+            else if (input_a > input_b) { return ">"; }
+            else return "<";
+        }
+        bool GroesserKleiner2(int input_a, int input_user)
+        {
+            if (input_a < input_user)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        bool GroesserKleiner3(int input_a, int input_user)
+        {
+            if (input_a > input_user)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        string GeradeUngerade(int input_a)
+        {
+            if (input_a % 2 == 0)
+            {
+                return "gerade";
+            }
+            else
+            {
+                return "ungerade";
+            }
+        }
+        int Kettenaufgabe1(int input_a, int input_b, int input_c) { return (input_a - input_b - input_c); }
+        int Kettenaufgabe2(int input_a, int input_b, int input_c) { return (input_a + input_b + input_c); }
+        int Kettenaufgabe3(int input_a, int input_b, int input_c) { return (input_a + input_b - input_c); }
+        int Kettenaufgabe4(int input_a, int input_b, int input_c) { return (input_a - input_b + input_c); }
+        int Kettenaufgabe5(int input_a, int input_b, int input_c) { return (input_a * input_b + input_c); }
+        int Kettenaufgabe6(int input_a, int input_b, int input_c) { return (input_a * input_b - input_c); }
+        int Kettenaufgabe7(int input_a, int input_b, int input_c, int input_d) { return (input_a + input_b - input_c + input_d); }
+
+        int VerdoppelHalbieren1(int input_a) { return input_a * 2; }
+        int VerdoppelHalbieren2(int input_a) { return input_a / 2; }
+
+
     }
 }
--- MadMaths/CalcFunctions_Mittelstufe.cs
+++ MadMaths/CalcFunctions_Mittelstufe.cs
@@ -6,11 +6,37 @@ using System.Threading.Tasks;
 
 namespace MadMaths
 {
-    class CalcFunctions_Mittelstufe
+    public class CalcFunctions_Mittelstufe
     {
-        int Variablenrechner(string a) {
+        int Variablen1(int input_a, int input_b) { return input_b - input_a; }
+        int Variablen2(int input_a, int input_b) { return input_b + input_a; }
+        int Variablen3(int input_a, int input_b) { return input_b / input_a; }
+        int Variablen4(int input_a, int input_b) { return input_b * input_a; }
+        string Variablen5(int input_a, int input_b, int input_c, int input_d)
+        {
+            int ergebnis_a = input_a + input_c;
+            string ergebnis = ergebnis_a + "x";
+            int ergebnis_b = input_b + input_d;
+            ergebnis = ergebnis + " + " + ergebnis_b + "y";
 
-
+            return ergebnis;
         }
+        string Variablen6(int input_a, int input_b, int input_c, int input_d)
+        {
+            int ergebnis_a = input_a - input_c;
+            string ergebnis = ergebnis_a + "x";
+            int ergebnis_b = input_b + input_d;
+            ergebnis = ergebnis + " + " + ergebnis_b + "y";
+
+            return ergebnis;
+        }
+        int Variablen7(int input_a, int input_b){ return input_a + input_b; }
+        int Variablen8(int input_a, int input_b,int input_c) { return input_a * input_c + input_b * input_c; }
+
+
+
+
+
+
     }
 }
