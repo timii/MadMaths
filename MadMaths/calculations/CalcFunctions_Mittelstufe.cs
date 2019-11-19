1dba28cc8d88a4b448f7dfa802035ac5ba1a0cab
Tim Thomas <inf3253@bsl-pc04.CAMPUS>
2019-11-19 15:10:49 +0100

    custommessagbox angepasst

--- MadMaths/calculations/CalcFunctions_Mittelstufe.cs
+++ MadMaths/calculations/CalcFunctions_Mittelstufe.cs
@@ -11,20 +11,26 @@ namespace MadMaths
 {
     public class CalcFunctions_Mittelstufe
     {
-       private double hoch(double input_a, double hochzahl)
+
+        static public Dictionary<string, Delegate> ms_funcs = new Dictionary<string, Delegate>()
+        {
+            {"Variablen1", new Func<int,int,int>(Variablen1)},
+        };
+
+        private double hoch(double input_a, double hochzahl)
         {
             double ergebnis = 1;
-            for(int i = 0; i < hochzahl; i++)
+            for (int i = 0; i < hochzahl; i++)
             {
                 ergebnis *= input_a;
             }
             return ergebnis;
         }
-       public int Variablen1(int input_a, int input_b) { return input_b - input_a; }
-       public int Variablen2(int input_a, int input_b) { return input_b + input_a; }
-       public int Variablen3(int input_a, int input_b) { return input_b / input_a; }
-       public int Variablen4(int input_a, int input_b) { return input_b * input_a; }
-       public string Variablen5(int input_a, int input_b, int input_c, int input_d)
+        public int Variablen1(int input_a, int input_b) { return input_b - input_a; }
+        public int Variablen2(int input_a, int input_b) { return input_b + input_a; }
+        public int Variablen3(int input_a, int input_b) { return input_b / input_a; }
+        public int Variablen4(int input_a, int input_b) { return input_b * input_a; }
+        public string Variablen5(int input_a, int input_b, int input_c, int input_d)
         {
             int ergebnis_a = input_a + input_c;
             string ergebnis = ergebnis_a + "x";
@@ -33,7 +39,7 @@ namespace MadMaths
 
             return ergebnis;
         }
-       public string Variablen6(double input_a, double input_b, double input_c, double input_d)
+        public string Variablen6(double input_a, double input_b, double input_c, double input_d)
         {
             double ergebnis_a = input_a - input_c;
             string ergebnis = ergebnis_a + "x";
@@ -42,16 +48,16 @@ namespace MadMaths
 
             return ergebnis;
         }
-       public double Variablen7(double input_a, double input_b){ return input_a + input_b; }
-       public double Variablen8(double input_a, double input_b, double input_c) { return input_a * input_c + input_b * input_c; }
-       public double Gleichungen1(double input_a, double input_b) { return input_b - input_a; }
-       public double Gleichungen2(double input_a, double input_b) { return input_b - input_a; }
-       public double Gleichungen3(double input_a, double input_b) { return input_a + input_b; }
-       public double Gleichungen4(double input_a, double input_b, double input_c) { return input_c - input_a - input_b; }
-       public double Gleichungen5(double input_a, double input_b, double input_c, double input_d) { return input_a - input_b + input_c - input_d; }
-       public double Gleichungen6(double input_a, double input_b) {return input_b / input_a; }
-       public double Gleichungen7(double input_a, double input_b, double input_c, double input_d) { return (input_d - (input_b * input_c)) / input_a; }
-       public double Gleichungen8(double input_a, double input_b, double input_c, double input_d)
+        public double Variablen7(double input_a, double input_b) { return input_a + input_b; }
+        public double Variablen8(double input_a, double input_b, double input_c) { return input_a * input_c + input_b * input_c; }
+        public double Gleichungen1(double input_a, double input_b) { return input_b - input_a; }
+        public double Gleichungen2(double input_a, double input_b) { return input_b - input_a; }
+        public double Gleichungen3(double input_a, double input_b) { return input_a + input_b; }
+        public double Gleichungen4(double input_a, double input_b, double input_c) { return input_c - input_a - input_b; }
+        public double Gleichungen5(double input_a, double input_b, double input_c, double input_d) { return input_a - input_b + input_c - input_d; }
+        public double Gleichungen6(double input_a, double input_b) { return input_b / input_a; }
+        public double Gleichungen7(double input_a, double input_b, double input_c, double input_d) { return (input_d - (input_b * input_c)) / input_a; }
+        public double Gleichungen8(double input_a, double input_b, double input_c, double input_d)
         {
             double variablenseite;
             double nummernseite;
@@ -59,7 +65,7 @@ namespace MadMaths
             {
                 variablenseite = input_a - input_d;
                 nummernseite = input_b * input_c;
-                   
+
             }
             else
             {
@@ -72,10 +78,10 @@ namespace MadMaths
         public string Gleichungssystem1(double input_a, double input_b, double input_c, double input_d, double input_e, double input_f)
         {
             double y = ((input_c / input_a) - (input_f / input_d)) / ((input_b / input_a) - (input_e / input_d));
-            double x = (input_c - input_b*y) / input_a;
+            double x = (input_c - input_b * y) / input_a;
 
             return "x = " + x + " \ny = " + y;
-            
+
         }
         public string Gleichungssystem2(double input_a, double input_b, double input_c, double input_d, double input_e, double input_f)
         {
@@ -103,15 +109,15 @@ namespace MadMaths
         public double Stochastik1() { return (1 / 6); }
         public double Stochastik2() { return (2 / 6); }
         public double Stochastik3() { return (3 / 6); }
-        public double Stochastik4() { return (3 /6); }
+        public double Stochastik4() { return (3 / 6); }
         public double Stochastik5() { return (1 / 4); }
 
         public double Dreisatz1(double input_a, double input_b, double input_c) { return ((input_b * input_c) / input_a); }
         public double Dreisatz2(double input_a, double input_b, double input_c) { return ((input_b * input_c) / input_a); }
         public double Dreisatz3(double input_a, double input_b, double input_c) { return ((input_b * input_c) / input_a); }
-        
-        public double Prozentrechnung1(double input_a, double input_b, double input_c) {return Math.Round(input_b*hoch((input_a * 0.01 + 1), input_c),2);}
-        public double Prozentrechnung2(double input_a, double input_b) { return Math.Round(input_a*(input_b*0.01), 2); }
+
+        public double Prozentrechnung1(double input_a, double input_b, double input_c) { return Math.Round(input_b * hoch((input_a * 0.01 + 1), input_c), 2); }
+        public double Prozentrechnung2(double input_a, double input_b) { return Math.Round(input_a * (input_b * 0.01), 2); }
 
     }
 }
