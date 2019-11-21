f55da7df43f1a31f45fa001daff9b343b7f3a562
Juzcan <56765047+Juzcan@users.noreply.github.com>
2019-11-21 15:03:33 +0100

    Ableiten 6 hinzugefügt

--- MadMaths/calculations/CalcFunctions_Oberstufe.cs
+++ MadMaths/calculations/CalcFunctions_Oberstufe.cs
@@ -61,8 +61,21 @@ namespace MadMaths.calculations
             return (input_a * input_b) + "x^" + (input_b-1) + " + " + (input_c * input_d) + "x^" + (input_d-1) + " + " + (input_e * input_f) + "x^" + (input_f -1);
 
         }
+        static public string Ableiten4(double input_a, double input_b, double input_c, double input_d, double input_e)
+        {
+            double vorx;
+            if (input_b == input_d)
+            {
+                vorx = (input_a + input_b)*input_e;
+                return (vorx * (input_b+1)) + "x^" + (input_b);
+            }
+            else
+            {
+                return ((input_a * input_e) * input_b) + "x^" + (input_b) + " + " + (input_c * input_e * input_d) + "x^" + (input_d);
 
-        static public string Ableiten5(double var1, double pot1, double var2, double pot2, double var3, double pot3, double var4, double pot4)
+            }
+        }
+        /*static public string Ableiten5(double var1, double pot1, double var2, double pot2, double var3, double pot3, double var4, double pot4)
         {
             double x1, x2, x3, x4;
             double xpot1, xpot2, xpot3, xpot4;
@@ -70,6 +83,19 @@ namespace MadMaths.calculations
             x2 = var1 * var4;
             x3 = var2 * var3;
             x4 = var2 * var4;
+        }*/
+
+        static public string Ableiten6(double input_a, double input_b, double input_c, double input_d, double input_e, double input_f)
+        {
+            if ((input_e)  == (input_b))
+            {
+                return "(" + (input_a * input_b * input_d - input_d * input_e * input_a) + "x^(" + (input_b + input_e - 1) + ") + (" + (input_a*input_b*input_f-input_d*input_c*input_e) + "x^(" + (input_e-1) + "))/"
+                    + "(" + (input_d * input_d) + "e^(" + (input_e * 2) + ") + " + (input_d * 2 * input_f) + "x^(" + (input_e) + ") + " + (input_f * input_f) + ")";
+
+            }
+            return "(" + (input_a * input_b * input_d - input_d * input_e * input_a) + "x^(" + (input_b + input_e - 1) + ") - " + (input_a * input_b * input_f) + "x^(" + (input_b - 1) + ") + " + (input_d * input_c * input_e) + "x^(" + (input_e - 1) + "))/"
+                + "(" + (input_d * input_d) + "e^(" + (input_e * 2) + ") + " + (input_d * 2 * input_f) + "x^(" + (input_e) + ") + " + (input_f * input_f) + ")";
+
         }
 
     }
