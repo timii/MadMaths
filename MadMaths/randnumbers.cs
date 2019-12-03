74e55cafc78b21e0f9d9ea9f2a94ea0355cfdb50
Juzcan <56765047+Juzcan@users.noreply.github.com>
2019-12-03 15:02:56 +0100

    Eingrenzung des Zahlenbereichs.Fertig.1
    (hoffentlich) fertigstellung der Eingrenzung + verschönerung

--- MadMaths/randnumbers.cs
+++ MadMaths/randnumbers.cs
@@ -12,15 +12,30 @@ namespace MadMaths
         static public object[] Zahlen(int argnum, string Thema, string Aufgabe)
         {
             Random rand = new Random();
-            int minValue;
+            int minValue = 1;
             int maxValue;
             object[] Zahl_Rückgabe = new object[argnum];
             switch(Thema) {
-                case "Kettenaufgaben": minValue = 1; maxValue = 10; break;
-                case "Zahlen Runden": minValue = 1; maxValue = 10000; break;
-                case "Textaufgaben II": minValue = 1; maxValue = 10; break;
+                case "Kettenaufgaben":  maxValue = 10; break;
+                case "Zahlen Runden":  maxValue = 10000; break;
+                case "TextaufgabenII":  maxValue = 10; break;
+                case "Stochastik":  maxValue = 6; break;
+                case "BinomischeFormeln":  maxValue = 25; break;
+                case "Wurzeln":  maxValue = 25; break;
+                case "Quadratische Gleichungen": maxValue = 25; break;
+                case "Potenzen": maxValue = 10; break;
+                case "Urnenmodell": maxValue = 15; break;
+                case "Hypergeometrische Verteilung": maxValue = 40; break;
+
                 default: minValue = 1; maxValue = 100; break;
             }
+            switch (Aufgabe)
+            {
+                case "Mittelwert1": maxValue = 1000;break;
+                case "Mittelwert2": maxValue = 5;break;
+                default: break;
+            }
+
             if (Aufgabe == "Subtrahieren 1") {
                 Zahl_Rückgabe[0] = rand.Next(minValue, maxValue);
                 do
@@ -59,6 +74,7 @@ namespace MadMaths
                 {
                     Zahl_Rückgabe[1] = rand.Next(minValue, maxValue);
                 } while (Int32.Parse(Zahl_Rückgabe[0].ToString()) < Int32.Parse(Zahl_Rückgabe[1].ToString()));
+                return Zahl_Rückgabe;
             }
             if (Aufgabe == "VerdoppelHalbieren2")
             {
@@ -75,15 +91,50 @@ namespace MadMaths
                 } while (Int32.Parse(Zahl_Rückgabe[0].ToString()) % 4 != 0);
                 return Zahl_Rückgabe;
             }
+            if (Thema == "Wurzeln")
+            {
+                Zahl_Rückgabe[0] = rand.Next(minValue, maxValue);
+                Zahl_Rückgabe[0] = Int32.Parse(Zahl_Rückgabe[0].ToString()) * Int32.Parse(Zahl_Rückgabe[0].ToString());
+                return Zahl_Rückgabe;
+            }
 
+            if (Aufgabe == "Urnenmodell1" || Aufgabe == "Urnenmodell2")
+            {
+                Zahl_Rückgabe[0] = rand.Next(minValue, maxValue);
+                do
+                {
+                    Zahl_Rückgabe[1] = rand.Next(minValue, maxValue);
+                } while (Int32.Parse(Zahl_Rückgabe[0].ToString()) < Int32.Parse(Zahl_Rückgabe[1].ToString())) ;
+                return Zahl_Rückgabe;
 
-
-            for (int i = 0; i < argnum; i++) {
-                Zahl_Rückgabe[i] = rand.Next(minValue, maxValue);
+            }
+            if (Thema == "Hypergeometrische Verteilung")
+            {
+                
+                Zahl_Rückgabe[0] = rand.Next(minValue, maxValue);
+                do
+                {
+                    Zahl_Rückgabe[1] = rand.Next(minValue, maxValue);
+                } while (Int32.Parse(Zahl_Rückgabe[0].ToString()) < Int32.Parse(Zahl_Rückgabe[1].ToString()));
+                do
+                {
+                    Zahl_Rückgabe[2] = rand.Next(minValue, maxValue);
+                } while (Int32.Parse(Zahl_Rückgabe[0].ToString()) < Int32.Parse(Zahl_Rückgabe[2].ToString()));
+                do
+                {
+                    Zahl_Rückgabe[3] = rand.Next(minValue, maxValue);
+                } while (Int32.Parse(Zahl_Rückgabe[2].ToString()) < Int32.Parse(Zahl_Rückgabe[3].ToString()) || Int32.Parse(Zahl_Rückgabe[1].ToString()) < Int32.Parse(Zahl_Rückgabe[3].ToString()));
+                for (int i = 4; i < argnum; i++)
+                {
+                    Zahl_Rückgabe[i] = 5;
+                }
+                return Zahl_Rückgabe;
 
             }
 
-
+            for (int i = 0; i < argnum; i++) {
+                Zahl_Rückgabe[i] = rand.Next(minValue, maxValue);
+            }
             return Zahl_Rückgabe;
             
 
